f62988ea136f65db9ed85038593dd74be1ae8b8f
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-07-12 20:19:50 +0530

    Create Collections_deque.py

---
+++ hackerrank/Collections/Collections_deque.py
@@ -0,0 +1,17 @@
+
+from collections import deque
+if __name__=='__main__':
+    d=deque()
+    n=int(input())
+    for _ in range(n):  
+        list=input().split()
+        if list[0]=="append":
+            d.append(list[1])
+        elif list[0]=="appendleft":
+            d.appendleft(list[1])
+        elif list[0]=="pop":
+            d.pop()
+        elif list[0]=="popleft":
+            d.popleft()
+            #the answer will be in the form of the list so to print the answer without list use *
+    print(*d)
